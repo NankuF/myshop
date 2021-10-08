5d647e9fa623f01866369b9555c36cc1b7fe7909
nankuF <fireloki87@gmail.com>
2021-10-08 20:32:44 +0300

    -shop -cart

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('-name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    # в админке есть prepopulated_fields, возможно тут автоподстановка слаг не нужна?
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.name)
+    #     super(Category, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.CharField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        # Определим индекс по двум полям, id и slug, с помощью атрибута index_together.
+        # Так как в дальнейшем мы будем запрашивать товары по их id и слагу,
+        # добавление такого индекса ускорит выборку объектов.
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.name)
+    #     super(Product, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+from cart.forms import CartAddProductForm
 
-# Create your views here.
+
+def product_list(request, category_slug=None):
+    """Список товаров, либо отфильтрованный список товаров"""
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    # если в url указан category_slug, фильтруем товар по категории
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+def product_detail(request, id, slug):
+    """Страница каждого товара с подробным описанием
+
+    slug в url позволяет генерировать хорошие с точки зрения SEO ссылки,
+    что улучшает индексацию сайта поисковыми роботами
+    """
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product,
+                                                        'cart_product_form': cart_product_form})
