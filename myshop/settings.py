ec01f2eb65235245f8a6aa7759bd3f25a15257fb
nankuF <fireloki87@gmail.com>
2021-10-20 16:27:44 +0300

    -payment

--- myshop/settings.py
+++ myshop/settings.py
@@ -13,12 +13,10 @@ import os.path
 from pathlib import Path
 import environ
 
-
 env = environ.Env()
 env.read_env('.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -30,7 +28,6 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -44,6 +41,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payment.apps.PaymentConfig',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myshop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -88,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -108,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -121,7 +116,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -139,3 +133,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'  # ключ по которому хранятся данные корзины в сессии
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Настройки Braintree
+BRAINTREE_MERCHANT_ID = env.str('BRAINTREE_MERCHANT_ID')
+BRAINTREE_PUBLIC_KEY = env.str('BRAINTREE_PUBLIC_KEY')
+BRAINTREE_PRIVATE_KEY = env.str('BRAINTREE_PRIVATE_KEY')
+
+from braintree import Configuration, Environment
+
+Configuration.configure(Environment.Sandbox,
+                        BRAINTREE_MERCHANT_ID,
+                        BRAINTREE_PUBLIC_KEY,
+                        BRAINTREE_PRIVATE_KEY)
